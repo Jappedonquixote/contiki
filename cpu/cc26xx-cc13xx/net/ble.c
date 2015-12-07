6bf1dcd235a92bb2fd67402c1d7e6e7268dc0789
spoerk <m.spoerk@student.tugraz.at>
2015-12-07 18:27:44 +0100

    nc advertisement test works rudimentary

--- cpu/cc26xx-cc13xx/net/ble.c
+++ cpu/cc26xx-cc13xx/net/ble.c
@@ -7,19 +7,74 @@
 
 #include "ble.h"
 #include "net/netstack.h"
+#include "net/linkaddr.h"
+#include "rf-core/rf-core.h"
+#include "rf-core/api/ble_cmd.h"
+#include "rf-core/api/common_cmd.h"
 
+#include <stdio.h>
+#include <string.h>
 /*---------------------------------------------------------------------------*/
 #define DEBUG 1
 #if DEBUG
-#include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
 #else
 #define PRINTF(...)
 #endif
 
+static unsigned char ble_params_buf[32];
+
 int ble_nc_advertisement(void)
 {
+	uint32_t cmd_status;
+	rfc_CMD_BLE_ADV_NC_t cmd;
+	rfc_bleAdvPar_t *params;
+
+	uint8_t channel = 37;
+	char name[] = "test payload - msp@7.12.15";
+	uint8_t *adv_payload = (uint8_t *) name;
+	uint8_t adv_payload_len = strlen(name);
+
 	PRINTF("ble_nc_advertisement called\n");
-//	NETSTACK_RADIO.send(0, 0);
-	return 0;
+
+	params = (rfc_bleAdvPar_t *)ble_params_buf;
+
+	/* Clear both buffers */
+	memset(&cmd, 0x00, sizeof(cmd));
+	memset(ble_params_buf, 0x00, sizeof(ble_params_buf));
+
+	/* Adv NC */
+	cmd.commandNo = CMD_BLE_ADV_NC;
+	cmd.condition.rule = COND_NEVER;
+	cmd.whitening.bOverride = 0;
+	cmd.whitening.init = 0;
+	cmd.pParams = params;
+	cmd.channel = channel;
+
+	/* Set up BLE Advertisement parameters */
+	params->pDeviceAddress = (uint16_t *) &linkaddr_node_addr.u8[LINKADDR_SIZE - 2];
+	params->endTrigger.triggerType = TRIG_NEVER;
+	params->endTime = TRIG_NEVER;
+
+	/* Set up BLE Advertisement parameters */
+	params = (rfc_bleAdvPar_t *) ble_params_buf;
+	params->advLen = adv_payload_len;
+	params->pAdvData = adv_payload;
+
+	if (rf_core_send_cmd((uint32_t) & cmd, &cmd_status) == RF_CORE_CMD_ERROR) {
+		PRINTF("send_ble_adv_nc: Chan=%d CMDSTA=0x%08lx, status=0x%04x\n",
+				channel, cmd_status, cmd.status);
+		return RF_CORE_CMD_ERROR;
+	}
+
+	/* Wait until the command is done */
+	if (rf_core_wait_cmd_done(&cmd) != RF_CORE_CMD_OK) {
+		PRINTF("send_ble_adv_nc: Chan=%d CMDSTA=0x%08lx, status=0x%04x\n",
+				channel, cmd_status, cmd.status);
+		return RF_CORE_CMD_ERROR;
+	}
+
+	PRINTF("ble_nc_advertisement finished\n");
+
+	return RF_CORE_CMD_OK;
 }
--- cpu/cc26xx-cc13xx/rf-core/ble-mode.c
+++ cpu/cc26xx-cc13xx/rf-core/ble-mode.c
@@ -1,4 +1,31 @@
+/**
+ * \file
+ * 		   Bluetooth LE radio driver
+ * \author
+ *         Michael Spoerk <m.spoerk@student.tugraz.at>
+ */
+
+#include "contiki.h"
 #include "dev/radio.h"
+#include "dev/cc26xx-uart.h"
+#include "dev/oscillators.h"
+
+#include "lpm.h"
+
+#include "ti-lib.h"
+#include "rf-core/rf-core.h"
+#include "rf-core/rf-ble.h"
+/*---------------------------------------------------------------------------*/
+/* RF core and RF HAL API */
+#include "hw_rfc_dbell.h"
+#include "hw_rfc_pwr.h"
+/*---------------------------------------------------------------------------*/
+/* RF Core Mailbox API */
+#include "rf-core/api/mailbox.h"
+#include "rf-core/api/common_cmd.h"
+#include "rf-core/api/ieee_cmd.h"
+#include "rf-core/api/data_entry.h"
+#include "rf-core/api/ieee_mailbox.h"
 
 /*---------------------------------------------------------------------------*/
 #define DEBUG 1
@@ -10,12 +37,86 @@
 #endif
 
 /*---------------------------------------------------------------------------*/
+typedef struct default_ble_tx_power_s {
+   uint16_t ib:6;
+   uint16_t gc:2;
+   uint16_t boost:1;
+   uint16_t temp_coeff:7;
+} default_ble_tx_power_t;
+
+static default_ble_tx_power_t tx_power = { 0x29, 0x00, 0x00, 0x00 };
+/*---------------------------------------------------------------------------*/
+/* BLE overrides */
+static uint32_t ble_overrides[] = {
+  0x00364038, /* Synth: Set RTRIM (POTAILRESTRIM) to 6 */
+  0x000784A3, /* Synth: Set FREF = 3.43 MHz (24 MHz / 7) */
+  0xA47E0583, /* Synth: Set loop bandwidth after lock to 80 kHz (K2) */
+  0xEAE00603, /* Synth: Set loop bandwidth after lock to 80 kHz (K3, LSB) */
+  0x00010623, /* Synth: Set loop bandwidth after lock to 80 kHz (K3, MSB) */
+  0x00456088, /* Adjust AGC reference level */
+  0xFFFFFFFF, /* End of override list */
+};
+/*---------------------------------------------------------------------------*/
+static int on(void);
+static int off(void);
+
+/*---------------------------------------------------------------------------*/
+/**
+ * \brief Checks whether the RFC domain is accessible and the RFC is in IEEE RX
+ * \return 1: RFC in RX mode (and therefore accessible too). 0 otherwise
+ */
+static uint8_t
+rf_is_on(void)
+{
+	if(!rf_core_is_accessible()) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+//  return RF_RADIO_OP_GET_STATUS(cmd_ieee_rx_buf) == RF_CORE_RADIO_OP_STATUS_ACTIVE;
+}
+/*---------------------------------------------------------------------------*/
+static uint8_t
+request(void)
+{
+	/*
+	 * We rely on the RDC layer to turn us on and off. Thus, if we are on we
+	 * will only allow sleep, standby otherwise
+	 */
+	if(rf_is_on()) {
+		return LPM_MODE_SLEEP;
+	}
+
+	return LPM_MODE_MAX_SUPPORTED;
+}
+/*---------------------------------------------------------------------------*/
+LPM_MODULE(cc26xx_rf_lpm_module, request, NULL, NULL, LPM_DOMAIN_NONE);
+/*---------------------------------------------------------------------------*/
 static int
 init(void)
 {
 	PRINTF("ble-mode init\n");
-	return 0;
+	lpm_register_module(&cc26xx_rf_lpm_module);
+
+	rf_core_set_modesel();
+
+	/* Populate the RF parameters data structure with default values */
+//	init_rf_params();
+
+	if(on() != RF_CORE_CMD_OK) {
+		PRINTF("init: on() failed\n");
+		return RF_CORE_CMD_ERROR;
+	}
+
+	ENERGEST_ON(ENERGEST_TYPE_LISTEN);
+//
+//	rf_core_primary_mode_register(&mode_ble);
+
+	process_start(&rf_core_process, NULL);
+	return 1;
 }
+
 /*---------------------------------------------------------------------------*/
 static int
 prepare(const void *payload, unsigned short payload_len)
@@ -67,18 +168,89 @@ pending_packet(void)
 	return 0;
 }
 /*---------------------------------------------------------------------------*/
+static uint8_t
+rf_radio_setup()
+{
+	uint32_t cmd_status;
+	rfc_CMD_RADIO_SETUP_t cmd;
+
+	/* Create radio setup command */
+	rf_core_init_radio_op((rfc_radioOp_t *)&cmd, sizeof(cmd), CMD_RADIO_SETUP);
+
+	cmd.txPower.IB = tx_power.ib;
+	cmd.txPower.GC = tx_power.gc;
+	cmd.txPower.tempCoeff = tx_power.temp_coeff;
+	cmd.txPower.boost = tx_power.boost;
+	cmd.pRegOverride = ble_overrides;
+	cmd.mode = 0;
+
+	/* Send Radio setup to RF Core */
+	if(rf_core_send_cmd((uint32_t)&cmd, &cmd_status) != RF_CORE_CMD_OK) {
+		PRINTF("rf_radio_setup: CMDSTA=0x%08lx, status=0x%04x\n",
+				cmd_status, cmd.status);
+		return RF_CORE_CMD_ERROR;
+	}
+
+	/* Wait until radio setup is done */
+	if(rf_core_wait_cmd_done(&cmd) != RF_CORE_CMD_OK) {
+		PRINTF("rf_radio_setup: wait, CMDSTA=0x%08lx, status=0x%04x\n",
+           cmd_status, cmd.status);
+		return RF_CORE_CMD_ERROR;
+	}
+
+	return RF_CORE_CMD_OK;
+}
+/*---------------------------------------------------------------------------*/
 static int
 on(void)
 {
 	PRINTF("ble-mode on\n");
-	return 0;
+	/*
+	 * Request the HF XOSC as the source for the HF clock. Needed before we can
+	 * use the FS. This will only request, it will _not_ perform the switch.
+	 */
+	oscillators_request_hf_xosc();
+
+	if(rf_is_on()) {
+		return RF_CORE_CMD_OK;
+	}
+
+	if(rf_core_boot() != RF_CORE_CMD_OK) {
+		PRINTF("on: rf_core_boot() failed\n");
+		return RF_CORE_CMD_ERROR;
+	}
+
+	rf_core_setup_interrupts();
+
+	/*
+	 * Trigger a switch to the XOSC, so that we can subsequently use the RF FS
+	 * This will block until the XOSC is actually ready, but give how we
+	 * requested it early on, this won't be too long a wait/
+	 */
+	oscillators_switch_to_hf_xosc();
+
+	if(rf_radio_setup() != RF_CORE_CMD_OK) {
+		PRINTF("on: radio_setup() failed\n");
+		return RF_CORE_CMD_ERROR;
+	}
+
+	return RF_CORE_CMD_OK;
 }
 /*---------------------------------------------------------------------------*/
 static int
 off(void)
 {
 	PRINTF("ble-mode off\n");
-	return 0;
+
+//	while (transmitting());
+	rf_core_power_down();
+
+	ENERGEST_OFF(ENERGEST_TYPE_LISTEN);
+
+	/* Switch HF clock source to the RCOSC to preserve power */
+	oscillators_switch_to_hf_rc();
+
+	return RF_CORE_CMD_OK;
 }
 /*---------------------------------------------------------------------------*/
 static radio_result_t
